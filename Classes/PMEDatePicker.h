63697eb09104241d7dd0786730d4dd6cafa29a4b
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-07-21 13:21:46 -0500

    Added `PMEDatePickerHandler`

--- Classes/PMEDatePicker.h
+++ Classes/PMEDatePicker.h
@@ -1,30 +1,37 @@
 //  Created by Michael Kamphausen on 06.11.13.
 //  Copyright (c) 2013 Michael Kamphausen. All rights reserved.
-//  Contribution (c) 2014 Sebastien REMY
 //
 
 #import <UIKit/UIKit.h>
 
 @class PMEDatePicker;
 
-@protocol PMEDatePickerDelegate <NSObject>
+typedef void (^PMEDatePickerHandler)(NSDate *date, NSTimeInterval seconds);
 
-- (void)datePicker:(PMEDatePicker*)datePicker didSelectDate:(NSDate*)date;
-
-@end
-
+@protocol PMEDatePickerDelegate;
 
 @interface PMEDatePicker : UIPickerView
 
-@property (nonatomic, retain) NSDate* date;
-@property (nonatomic, retain) NSDate* maximumDate;
-@property (nonatomic, retain) NSDate* minimumDate;
+@property (nonatomic, strong) NSDate* date;
+@property (nonatomic) NSTimeInterval seconds;
+@property (readonly, nonatomic) NSDate *referenceDate;
+@property (nonatomic, strong) NSDate* maximumDate;
+@property (nonatomic, strong) NSDate* minimumDate;
 @property (nonatomic, strong, readonly) NSDateFormatter* dateFormatter;
+@property (nonatomic) UIColor *textColor;
+@property (nonatomic) UIFont *textFont;
 //! Supported date symbols are yyyy, MMM, d, HH, h, mm, j, a. Default is 'yyyyMMMdjmm'. Order is determined by locale.
 @property (nonatomic, copy) NSString* dateFormatTemplate;
 //! Use this delegate instead of inherited delegate and dataSource properties
 @property (nonatomic, weak) IBOutlet id<PMEDatePickerDelegate> dateDelegate;
+@property (strong) PMEDatePickerHandler handler;
 
 - (void)setDate:(NSDate*)date animated:(BOOL)animated;
+
+@end
+
+@protocol PMEDatePickerDelegate <NSObject>
+
+- (void)datePicker:(PMEDatePicker*)datePicker didSelectDate:(NSDate*)date;
 
 @end
